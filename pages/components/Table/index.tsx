7676f05e859fc98fee485e3495da999619c0e654
Arian <arianandcode@gmail.com>
2023-01-09 20:45:35 +0800

    feat: add table

--- pages/components/Table/index.tsx
+++ pages/components/Table/index.tsx
@@ -3,10 +3,9 @@ import styles from './table.module.css'
 
 interface ITable {
   title: string;
-  colorType?: 'brand' | 'orange' | 'green' | 'red';
 }
 
-const Table: React.FC<ITable> = ({ title, colorType = 'brand' }) => {
+const Table: React.FC<ITable> = ({ title}) => {
   return <div className={styles.tag}>{title}</div>;
 };
 
