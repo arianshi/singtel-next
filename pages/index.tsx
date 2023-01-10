b456f8f26e82067f987a89c1275976b159017be9
Arian <arianandcode@gmail.com>
2023-01-11 00:52:30 +0800

    feat: add lerna

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Table from './components/Table';
+import Table from '../packages/Table';
 import styles from '../styles/Home.module.css';
 import { ReactElement, JSXElementConstructor, useState } from 'react';
 
