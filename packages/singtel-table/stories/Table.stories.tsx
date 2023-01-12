08195326999c6d7e4b385a62486b1800cd0ced17
Arian <arianandcode@gmail.com>
2023-01-12 17:26:37 +0800

    feat: add table storybook

--- packages/singtel-table/stories/Table.stories.tsx
+++ packages/singtel-table/stories/Table.stories.tsx
@@ -8,9 +8,7 @@ export default {
   title: 'Example/Table',
   component: Table,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } as ComponentMeta<typeof Table>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -18,24 +16,4 @@ const Template: ComponentStory<typeof Table> = (args) => <Table {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+Primary.args = {};
--- packages/singtel-table/stories/Table.tsx
+++ packages/singtel-table/stories/Table.tsx
@@ -1,52 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SingtelTable from 'singtel-table';
 import './table.css';
-
 interface TableProps {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  showHead?: boolean;
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Table = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: TableProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
+export const Table = ({ showHead = false, ...props }: TableProps) => {
+  const data = [
+    {
+      id: 1,
+      price: 170000,
+      operator: 'Aelcom Axiatavvv',
+      display: 'CELCOM / My Celcom / 502 19',
+      availability: 'Yes',
+    },
+    {
+      id: 2,
+      price: 250000,
+      operator: 'HiGiTelecom',
+      display: 'DiGi 1800 / DiGi /  MYMY18',
+      availability: 'Yes',
+    },
+    {
+      id: 3,
+      price: 160000,
+      operator: 'Kaxis (LTE)',
+      display: 'U Mobile / MYS 18 / MY 18',
+      availability: 'Yes',
+    },
+    {
+      id: 4,
+      price: 460000,
+      operator: 'DMobile (LTE)',
+      display: 'U Mobile / MYS 18 / MY 18',
+      availability: 'Yes',
+    },
+  ];
+  const columns = [
+    {
+      title: 'Operator',
+      key: 'operator',
+      dataIndex: 'operator',
+      sortDirections: ['descend'],
+      render: (e: any) => <span>{e}</span>,
+      sorter: (
+        a: { operator: string | any[] },
+        b: { operator: string | any[] }
+      ) => {
+        return a.operator.length - b.operator.length;
+      },
+    },
+    {
+      title: 'Price',
+      key: 'price',
+      dataIndex: 'price',
+      sortDirections: ['ascend'],
+      render: (e: any) => <span>{e}</span>,
+      sorter: (a: { price: number }, b: { price: number }) => {
+        return a.price - b.price;
+      },
+    },
+    {
+      title: 'Headset Display',
+      key: 'display',
+      dataIndex: 'display',
+      render: (e: any) => <span>{e}</span>,
+    },
+    {
+      title: '3G Availability',
+      key: 'availability',
+      dataIndex: 'availability',
+      render: (e: any) => <span>{e}</span>,
+    },
+  ];
+  const [type, setType] = useState('radio');
+  const [selectedRowKeys, setSelectedRowKeys] = useState(['0']);
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
-      )}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <SingtelTable
+      showHead
+      rowSelection={{
+        type,
+        selectedRowKeys,
+        onSelect: (selectedRowKeys: any, selectedRows: any) => {
+          console.log('selectedRowKeys:', selectedRowKeys, selectedRows);
+        },
+      }}
+      columns={columns}
+      data={data}
+    />
   );
 };
